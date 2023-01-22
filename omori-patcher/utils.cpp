ed96a538b61e65924ac785475ca5e35baee62054
nemtudom345 <nemtudom345@protonmail.com>
2023-01-22 10:36:24 +0100

    Migrated project to CMake

--- omori-patcher/utils.cpp
+++ omori-patcher/utils.cpp
@@ -8,25 +8,6 @@
 
 typedef unsigned int natural;
 
-typedef struct HookResult
-{
-    void* trampolinePtr;
-    void* backupPtr;
-};
-
-namespace Consts {
-    const int RESET = 7;
-    const int SUCCESS = 10;
-    const int INFO = 11;
-    const int ERR = 12;
-    const int WARN = 14;
-
-
-    const DWORD_PTR codecave = 0x0000000142BEC100;
-    const DWORD_PTR codecaveEnd = 0x0000000142BED0B5;
-    const DWORD_PTR JS_Eval = 0x00000001427776ED; // x64dbg actually thinks that this function is at: 0x1427776EC
-}
-
 namespace Utils
 {
     // https://stackoverflow.com/questions/311955/redirecting-cout-to-a-console-in-windows
