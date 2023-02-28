ed7ae93ed02a674ad47eca3289f3b28f66eea7bd
nemtudom345 <nemtudom345@protonmail.com>
2023-02-28 00:33:36 -0800

    Custom js execution

--- omori-patcher/dllmain.cpp
+++ omori-patcher/dllmain.cpp
@@ -6,27 +6,33 @@
 #include "consts.h"
 #include "Chowdren.exe.h"
 
+typedef void (*JS_EvalFunc)(JSContext* ctx, const char* buf, size_t buf_len, const char* filename, int eval_flags);
+
 JSContext* JSContextInst;
 JSRuntime* JSRuntimeInst;
 
-/*
+void JS_Eval(const char* code, const char* filename = "<omori-patcher>")
+{
+    auto JS_Eval = (JS_EvalFunc) Consts::JS_Eval;
+    JS_Eval(JSContextInst, code, strlen(code), filename, 0);
+}
+
 void JS_NewCFunctionHook(void* ctx, void* function, char* name, int length)
 {
     if (name != nullptr && *name != 0)
     {
-        Utils::Infof("[NewCFunction] JSContext* ctx = 0x%p, function*=%p, name*=%p, name=%s, length=%d", ctx, function, name, name, length);
+        // Utils::Infof("[NewCFunction] JSContext* ctx = 0x%p, function*=%p, name*=%p, name=%s, length=%d", ctx, function, name, name, length);
     }
 }
-*/
 
 void JS_DumpMemoryUsage(FILE* fp,JSMemoryUsage* s,JSRuntime* rt);
 
-void JS_EvalHook(void* ctx,char* buf,size_t buf_len,char* filename,int eval_flags)
+void JS_EvalHook(JSContext* ctx, const char* buf, size_t buf_len, const char* filename, int eval_flags)
 {
-    Utils::Infof("[eval] JSContext* ctx = %p,char* buf = %s,size_t buf_len = %d,char* filename = %s,int eval_flags = %d", ctx, buf, buf_len, filename, eval_flags);
+    Utils::Infof("[eval_safe] JSContext* ctx = %p,char* buf = %s,size_t buf_len = %d,char* filename = %s,int eval_flags = %d", ctx, buf, buf_len, filename, eval_flags);
 }
 
-void JS_EvalBinHook(void* ctx, char* filename)
+void JS_EvalBinHook(JSContext* ctx, char* filename)
 {
     Utils::Infof("[evalbin] JSContext* ctx = %p, filename = %s", ctx, filename);
 }
@@ -43,6 +49,8 @@ void PostEvalBinHook()
     Utils::Info("PostEvalBinHook");
     Utils::Infof("JSRuntime* rt = %p", JSRuntimeInst);
     Utils::Infof("JSContext* ctx = %p", JSContextInst);
+
+    JS_Eval("try{ console.log(JSON.stringify(globalThis)); } catch(ex) { console.log(ex); }");
 }
 
 void PatcherMain()
@@ -55,7 +63,7 @@ void PatcherMain()
 
     Utils::Success("DLL Successfully loaded!");
 
-    // Mem::Hook(Consts::JS_NewCFunction, Consts::JS_NewCFunctionOffset, (DWORD_PTR) &JS_NewCFunctionHook);
+    Mem::Hook(Consts::JS_NewCFunction, (DWORD_PTR) &JS_NewCFunctionHook, true);
     Mem::Hook(Consts::JS_Eval, (DWORD_PTR) &JS_EvalHook, true);
     Mem::Hook(Consts::JS_EvalBin, (DWORD_PTR) &JS_EvalBinHook, true);
     Mem::Hook(Consts::JSImpl_print_i, (DWORD_PTR) &PrintHook, true);
