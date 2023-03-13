37746ca156ba3d95f4f25d5b4fba9b6258560819
nemtudom345 <nemtudom345@protonmail.com>
2023-03-13 14:35:49 +0100

    Added support for adding files to fs overlay that don't exist on disk

--- omori-patcher/fs_overlay.cpp
+++ omori-patcher/fs_overlay.cpp
@@ -9,6 +9,9 @@ static HANDLE (WINAPI* trueCreateFileW)(LPCWSTR lpFileName, DWORD dwDesiredAcces
 static BOOL (WINAPI* trueReadFile)(HANDLE hFile, LPVOID lpBuffer, DWORD nNumberOfBytesToRead, LPDWORD lpNumberOfBytesRead, LPOVERLAPPED lpOverlapped) = ReadFile;
 
 std::map<std::wstring, std::wstring> overlay;
+std::map<std::wstring, FileData> binOverlay;
+std::map<HANDLE, std::wstring> fileMap;
+std::map<HANDLE, _LARGE_INTEGER> filePtrMap;
 std::mutex mapMutex;
 
 void FS_RegisterOverlay(const Mod& mod)
@@ -22,8 +25,8 @@ void FS_RegisterOverlay(const Mod& mod)
         const char* newS = Utils::GetAbsolutePath(newAsset.c_str());
         size_t s_newsize = strlen(s) + 1;
         size_t newS_newsize = strlen(newS) + 1;
-        wchar_t* s_wcstring = new wchar_t[s_newsize];
-        wchar_t* newS_wcstring = new wchar_t[newS_newsize];
+        auto s_wcstring = new wchar_t[s_newsize];
+        auto newS_wcstring = new wchar_t[newS_newsize];
         size_t convertedChars = 0;
         mbstowcs_s(&convertedChars, s_wcstring, s_newsize, s, _TRUNCATE);
         mbstowcs_s(&convertedChars, newS_wcstring, newS_newsize, newS, _TRUNCATE);
@@ -34,20 +37,77 @@ void FS_RegisterOverlay(const Mod& mod)
         free((void*) s);
         free((void*) s_wcstring);
     }
-    Utils::Infof("%d", overlay.size());
+}
+
+BOOL WINAPI hookedSetFilePointerEx(HANDLE hFile, _LARGE_INTEGER liDistanceToMove, PLARGE_INTEGER lpNewFilePointer, DWORD dwMoveMethod)
+{
+    std::lock_guard<std::mutex> lock(mapMutex);
+    auto filename = fileMap[hFile];
+    if (binOverlay.contains(filename))
+    {
+        if (!filePtrMap.contains(hFile)) filePtrMap[hFile] = _LARGE_INTEGER{};
+        switch (dwMoveMethod) {
+            case FILE_BEGIN:
+                filePtrMap[hFile].QuadPart = 0;
+                *lpNewFilePointer = filePtrMap[hFile];
+                break;
+            case FILE_CURRENT:
+                *lpNewFilePointer = filePtrMap[hFile];
+                break;
+            case FILE_END:
+                filePtrMap[hFile].QuadPart = binOverlay[filename].size;
+                *lpNewFilePointer = filePtrMap[hFile];
+                break;
+            default:
+                Utils::Warnf("Unsupported setFilePointerEx move method: %d", dwMoveMethod);
+        }
+        return true;
+    }
+    return trueSetFilePointerEx(hFile, liDistanceToMove, lpNewFilePointer, dwMoveMethod);
 }
 
 HANDLE WINAPI hookedCreateFileW(LPCWSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes, DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTemplateFile)
 {
     std::lock_guard<std::mutex> lock(mapMutex);
+    HANDLE handle;
     if (overlay.contains(Utils::GetAbsolutePathW(lpFileName)))
     {
-        return trueCreateFileW(overlay[Utils::GetAbsolutePathW(lpFileName)].c_str(), dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
+        handle = trueCreateFileW(overlay[Utils::GetAbsolutePathW(lpFileName)].c_str(), dwDesiredAccess, dwShareMode,
+                                 lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
     }
-    return trueCreateFileW(lpFileName, dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
+    else {
+        handle = trueCreateFileW(lpFileName, dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition,
+                                 dwFlagsAndAttributes, hTemplateFile);
+    }
+    fileMap[handle] = Utils::GetAbsolutePathW(lpFileName);
+    return handle;
+}
+
+BOOL WINAPI hookedReadFile(HANDLE hFile, LPVOID lpBuffer, DWORD nNumberOfBytesToRead, LPDWORD lpNumberOfBytesRead, LPOVERLAPPED lpOverlapped)
+{
+    std::lock_guard<std::mutex> lock(mapMutex);
+    auto filename = fileMap[hFile];
+    if (binOverlay.contains(filename))
+    {
+        if (lpOverlapped != nullptr)
+        {
+            Utils::Warn("lpOverlapped != nullptr on an overlayed file, thinks might break");
+        }
+        size_t len = nNumberOfBytesToRead;
+        size_t offset = filePtrMap[hFile].QuadPart;
+        if (len + offset > binOverlay[filename].size) len = len + offset - binOverlay[filename].size;
+        filePtrMap[hFile].QuadPart += nNumberOfBytesToRead;
+        if (filePtrMap[hFile].QuadPart >= binOverlay[filename].size) filePtrMap[hFile].QuadPart = 0;
+        memcpy(lpBuffer, binOverlay[filename].data+offset, len);
+        *lpNumberOfBytesRead = len;
+        return true;
+    }
+    return trueReadFile(hFile, lpBuffer, nNumberOfBytesToRead, lpNumberOfBytesRead, lpOverlapped);
 }
 
 void FS_RegisterDetours()
 {
     DetourAttach(&(PVOID &) trueCreateFileW, (PVOID) hookedCreateFileW);
+    DetourAttach(&(PVOID &) trueReadFile, (PVOID) hookedReadFile);
+    DetourAttach(&(PVOID &) trueSetFilePointerEx, (PVOID) hookedSetFilePointerEx);
 }
