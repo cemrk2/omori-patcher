19e6a45ca1a5cd8f65f4a0b337821e792ee13f39
nemtudom345 <nemtudom345@protonmail.com>
2023-03-18 16:32:40 +0100

    Image de/encoding
    Added support for decoding and re-encoding images that are targeted by olid

--- omori-patcher/fs_overlay.cpp
+++ omori-patcher/fs_overlay.cpp
@@ -1,5 +1,7 @@
 #include <mutex>
-#include <iostream>
+#include <sail-c++/sail-c++.h>
+#include <sail-c++/image_output-c++.h>
+#include <png.h>
 #include "fs_overlay.h"
 #include "utils.h"
 #include "detours.h"
@@ -113,6 +115,78 @@ void addFile(const Mod& mod, const Json::Value& v)
     free((void*) modAssetWCstr);
 }
 
+// https://gist.github.com/dobrokot/10486786
+static void PngWriteCallback(png_structp  png_ptr, png_bytep data, png_size_t length) {
+    auto* p = (std::vector<uint8_t>*)png_get_io_ptr(png_ptr);
+    p->insert(p->end(), data, data + length);
+}
+
+bool WritePngToMemory(size_t w, size_t h, const void* dataRGBA, std::vector<uint8_t> *out) {
+    out->clear();
+    png_structp p = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if(!p)
+    {
+        Utils::Error("png_create_write_struct() failed");
+        return false;
+    }
+
+    png_infop info_ptr = png_create_info_struct(p);
+    if (!info_ptr)
+    {
+        Utils::Error("png_create_info_struct() failed");
+        return false;
+    }
+    if (setjmp(png_jmpbuf(p)) != 0)
+    {
+        Utils::Error("setjmp(png_jmpbuf(p) failed");
+        return false;
+    }
+
+    png_set_IHDR(p, info_ptr, w, h, 8,
+                 PNG_COLOR_TYPE_RGBA,
+                 PNG_INTERLACE_NONE,
+                 PNG_COMPRESSION_TYPE_DEFAULT,
+                 PNG_FILTER_TYPE_DEFAULT);
+    std::vector<uint8_t*> rows(h);
+    for (size_t y = 0; y < h; ++y)
+        rows[y] = (uint8_t*)dataRGBA + y * w * 4;
+    png_set_rows(p, info_ptr, &rows[0]);
+    png_set_write_fn(p, out, PngWriteCallback, NULL);
+    png_write_png(p, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+
+    png_destroy_write_struct(&p, NULL);
+
+    return true;
+}
+
+void addOLID(const Mod& mod, const Json::Value& deltaObj)
+{
+    std::string target = deltaObj["patch"].asString();
+    std::string patch = "mods\\" + mod.modDir + "\\" + deltaObj["with"].asString();
+    const char* targetCStr = Utils::GetAbsolutePath(target.c_str());
+    size_t targetSize = strlen(targetCStr) + 1;
+    auto targetWStr = new wchar_t[targetSize];
+    size_t convertedChars = 0;
+    mbstowcs_s(&convertedChars, targetWStr, targetSize, targetCStr, _TRUNCATE);
+    auto olid = Utils::ReadFileData(patch.c_str());
+
+    sail::image image(target);
+    std::vector<uint8_t> imgVec(0);
+    image.convert(SAIL_PIXEL_FORMAT_BPP32_RGBA);
+
+    WritePngToMemory(image.width(), image.height(), image.pixels(), &imgVec);
+
+    auto* buff = (uint8_t*) malloc(imgVec.size());
+    memcpy(buff, imgVec.data(), imgVec.size());
+    binOverlay[std::wstring(targetWStr)] = FileData{
+        buff,
+        imgVec.size()
+    };
+
+    free(targetWStr);
+    free(olid.data);
+}
+
 void FS_RegisterOverlay(const Mod& mod)
 {
     std::vector<std::string> modKeys = {"assets", "files", "maps", "data"};
@@ -123,6 +197,15 @@ void FS_RegisterOverlay(const Mod& mod)
         for (const auto& v : arr)
         {
             addFile(mod, v);
+        }
+    }
+
+    auto image_deltas = mod.rawConfig.get("image_deltas", {});
+    if (!image_deltas.empty())
+    {
+        for (const Json::Value& delta : image_deltas)
+        {
+            addOLID(mod, delta);
         }
     }
 
